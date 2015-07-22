33d351cabf7eef4f3f02d8d5caf88dfa613d0789
narayaanir <anirudh_narayan111@yahoo.com>
2015-07-22 10:05:25 -0400

    Modified script to include client details.

--- myserver.h
+++ myserver.h
@@ -15,6 +15,28 @@ private:
         while(waitpid(-1, NULL, WNOHANG) > 0);
     }
 
+    void findClientDetails(int& r) {
+        socklen_t len;
+        struct sockaddr_storage addr;
+        char ipstr[INET6_ADDRSTRLEN];
+        int c_port;
+
+        len = sizeof addr;
+        getpeername(r, (struct sockaddr*)&addr, &len);
+
+        // deal with both IPv4 and IPv6:
+        if (addr.ss_family == AF_INET) {
+            struct sockaddr_in *s = (struct sockaddr_in *)&addr;
+            c_port = ntohs(s->sin_port);
+            inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
+        } else { // AF_INET6
+            struct sockaddr_in6 *s = (struct sockaddr_in6 *)&addr;
+            c_port = ntohs(s->sin6_port);
+            inet_ntop(AF_INET6, &s->sin6_addr, ipstr, sizeof ipstr);
+        }
+        std::cout << "Connected to client with IP " << ipstr << " on " << c_port << std::endl;
+    }
+
 public:
     // Constructor
     Myserver (std::string p) : Mysocket(p)
@@ -56,6 +78,7 @@ public:
             exit(1);
         }
 
+
         char s[INET6_ADDRSTRLEN];
         int new_fd; // listen on sock_fd, new connection on new_fd
         while(true)
@@ -69,6 +92,7 @@ public:
             inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
             if (!fork()) { // this is the child process
                 close(sockfd); // child doesn't need the listener
+                findClientDetails(new_fd);
                 if (send(new_fd, message.c_str(), message.length(), 0) == -1)
                     perror("send");
                 close(new_fd);
